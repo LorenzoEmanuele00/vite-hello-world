f11eadfc6a0c6838ec78fbc3238e462943b1717a
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-11-22 16:28:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import Title from './components/Title.vue';
+
+  export default {
+    components: {
+      Title,
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<template>
+    <Title></Title>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<script >
+  export default {
+    data() {
+      return {
+        text: "La mia prima app con vite",
+      };
+    },
+  };
+</script>
+
+<template>
+  <h2>{{ text }}</h2>
+</template>
+
+<style>
+</style>
